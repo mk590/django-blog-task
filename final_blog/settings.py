3cefab53be5bcdaf27102337b55dacb0496e3868
Mohit <lalpanda6@gmail.com>
2022-11-13 18:48:56 +0530

    the migration was successful after removing the comma from the end of the statidfiles dr in the setting file of the project

--- final_blog/settings.py
+++ final_blog/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'blog',
     'rest_framework',
     # 
-    'authors',
+    # 'authors',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# 
+MEDIA_URL='/media/'
+
+MEDIA_ROOT=[ BASE_DIR / 'media']
+
+STATICFILES_DIRS = (
+    [ BASE_DIR / 'static']
+    )
--- final_blog/urls.py
+++ final_blog/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
      path('author/', include('django.contrib.auth.urls')),
-    path('author/', include('authors.urls')),
-]
+    # path('author/', include('authors.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
