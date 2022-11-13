8305423f6743f8939e418c53d3be26fc52253370
Mohit <lalpanda6@gmail.com>
2022-11-13 19:06:18 +0530

    the image upload is working correctly after commenting the media root line it is working great

--- final_blog/settings.py
+++ final_blog/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # 
 MEDIA_URL='/media/'
 
-MEDIA_ROOT=[ BASE_DIR / 'media']
+# MEDIA_ROOT=[ BASE_DIR / 'media']
 
 STATICFILES_DIRS = (
     [ BASE_DIR / 'static']
